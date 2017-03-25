ea1c771ce0d2bec670e541228f71c906c999ae1c
Ben <benferree3@gmail.com>
2017-03-25 08:00:18 -0600

    Create generateLevel.js

---
+++ script/generateLevel.js
@@ -0,0 +1,45 @@
+var rangeOfRandomNumber = 0;
+var justFoundRandomNumber = 0;
+var theWallThatWeAreOn = 11;
+var GapNumber = 15;
+var theWallToHide;
+function randomNumber() {
+    justFoundRandomNumber = justFoundRandomNumber - justFoundRandomNumber;
+    justFoundRandomNumber = justFoundRandomNumber + Math.floor((Math.random() * rangeOfRandomNumber) + 1);
+}
+
+function startDraw(){
+        rangeOfRandomNumber = rangeOfRandomNumber - rangeOfRandomNumber;
+        rangeOfRandomNumber = rangeOfRandomNumber + 4;
+        randomNumber();
+
+        if (justFoundRandomNumber == 4) {
+            if (theWallThatWeAreOn == GapNumber) {
+                GapNumber = GapNumber + 8;
+                theWallThatWeAreOn = theWallThatWeAreOn + 3;
+                startDraw();
+            }
+            else {
+                theWallThatWeAreOn = theWallThatWeAreOn + 1;
+                startDraw();
+            }
+        }
+        else if (justFoundRandomNumber != 4) {
+            theWallToHide = "WALL" + theWallThatWeAreOn;
+            alert(theWallToHide);
+            hideTheWall();
+            theWallThatWeAreOn = theWallThatWeAreOn +1;
+            startDraw();
+        }
+}
+
+function hideTheWall(){
+    var wall = document.getElementById(theWallToHide);
+    console.log(wall);
+    console.log(theWallToHide);
+    wall.style.visibility = 'hidden';
+}
+//console.log(theWallToHide);
+// document.getElementById(theWallToHide).style.visibility = 'hidden';
+
+startDraw();
