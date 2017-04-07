d5268c1f9cc0b48c830fc7a05c38b2b9ba677ba1
Ben <benferree3@gmail.com>
2017-04-07 06:36:41 -0600

    Update generateLevel.js

--- script/generateLevel.js
+++ script/generateLevel.js
@@ -2,14 +2,14 @@ var rangeOfRandomNumber = 0;
 var justFoundRandomNumber = 0;
 var theWallThatWeAreOn = 11;
 var GapNumber = 15;
-var theWallToHide = "WALL1";
+var theWallToHide;
 function randomNumber() {
     justFoundRandomNumber = justFoundRandomNumber - justFoundRandomNumber;
     justFoundRandomNumber = justFoundRandomNumber + Math.floor((Math.random() * rangeOfRandomNumber) + 1);
 }
 
 function startDraw(){
-    if(theWallToHide < 48 ){
+    if(theWallThatWeAreOn < 48 ){
         rangeOfRandomNumber = rangeOfRandomNumber - rangeOfRandomNumber;
         rangeOfRandomNumber = rangeOfRandomNumber + 4;
         randomNumber();
@@ -26,8 +26,6 @@ function startDraw(){
             }
         }
         else if (justFoundRandomNumber != 4) {
-            delete theWallToHide;
-            var theWallToHide;
             theWallToHide = "WALL" + theWallThatWeAreOn;
             //alert(theWallToHide);
             hideTheWall();
@@ -44,11 +42,9 @@ function startDraw(){
 
 function hideTheWall(){
     var wall = document.getElementById(theWallToHide);
-    console.log(wall);
     console.log(theWallToHide);
     wall.style.visibility = 'hidden';
 }
-//console.log(theWallToHide);
-// document.getElementById(theWallToHide).style.visibility = 'hidden';
+
 
 startDraw();
